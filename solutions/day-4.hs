cdc300b7496067e1ebe2b9737116798f93ef9c7e
Luis Morillo <luis_cnnvd@hotmail.com>
2022-12-04 12:47:56 +0100

    new solutions with interval algebra

--- solutions/day-4.hs
+++ solutions/day-4.hs
@@ -1,16 +1,62 @@
 module Main where
 
 import System.Environment (getArgs)
+import Data.IntegerInterval
+import Data.IntervalRelation
+import Data.ExtendedReal
+import Data.Attoparsec.ByteString.Char8 (Parser)
+import qualified Data.Attoparsec.ByteString.Char8 as P
+import qualified Data.ByteString as BS
+
+parseIntervals :: Parser IntegerInterval
+parseIntervals = (<=..<=)
+             <$> (Finite <$> P.decimal)
+             <*> (P.char '-' >> Finite <$> P.decimal)
+
+
+parseIntervalPair :: Parser (IntegerInterval, IntegerInterval)
+parseIntervalPair = (,)
+                <$> parseIntervals
+                <*> (P.char ',' >> parseIntervals)
+
+parseInput :: Parser [(IntegerInterval, IntegerInterval)]
+parseInput = parseIntervalPair `P.sepBy` P.endOfLine
+
+wasted :: IntegerInterval -> IntegerInterval -> Bool
+wasted i j =
+  case i `relate` j of
+    Starts -> True
+    During -> True
+    Finishes -> True
+    Equal -> True
+    StartedBy -> True
+    Contains -> True
+    FinishedBy -> True
+    _ -> False
+
+wastedWithOverlap :: IntegerInterval -> IntegerInterval -> Bool
+wastedWithOverlap i j =
+  case i `relate` j of
+    Starts -> True
+    During -> True
+    Finishes -> True
+    Equal -> True
+    StartedBy -> True
+    Contains -> True
+    FinishedBy -> True
+    OverlappedBy -> True
+    Overlaps -> True
+    _ -> False
 
 main :: IO ()
 main = do
   [part, filepath] <- getArgs
-  input <- lines <$> readFile filepath
+  input <- BS.readFile filepath
   if read @Int part == 1
     then do
       print "solution to problem 1 is:"
-      print "not implemented"
+      print $ sum . fmap (fromEnum . uncurry wasted) <$> P.parseOnly parseInput input 
     else do
       print "solution to problem 2 is:"
-      print "not implemented"
+      print $ sum . fmap (fromEnum . uncurry wastedWithOverlap) <$> P.parseOnly parseInput input 
 
