024bd9f3ce4ea60f4e4abae1e0284701732a0c3f
Luis Morillo <luis_cnnvd@hotmail.com>
2022-12-10 16:24:59 +0100

    day 8

--- solutions/day-8.hs
+++ solutions/day-8.hs
@@ -1,16 +1,61 @@
 module Main where
 
 import System.Environment (getArgs)
+import Data.Char (digitToInt)
+import Data.Matrix (Matrix)
+import Data.Matrix qualified as M
+import Data.Monoid (Sum(Sum))
+import Data.Semigroup (Sum(getSum))
+
+isVisible :: Matrix Int -> Matrix Bool
+isVisible m = M.mapPos f m
+ where f :: ((Int, Int) -> Int -> Bool) 
+       f (r, c) current_elem = 
+        let nrows = M.nrows m
+            ncols = M.ncols m
+            check_left   = all (< current_elem) $ M.submatrix r     r     1     (c-1) m
+            check_right  = all (< current_elem) $ M.submatrix r     r     (c+1) ncols m
+            check_top    = all (< current_elem) $ M.submatrix 1     (r-1) c     c     m
+            check_bottom = all (< current_elem) $ M.submatrix (r+1) nrows c     c     m
+         in r `elem` [1, nrows] || c `elem` [1, ncols] || check_left || check_right || check_top || check_bottom
+
+sceneScore :: Matrix Int -> Matrix Int
+sceneScore m = M.mapPos f m
+ where f :: (Int, Int) -> Int -> Int
+       f (r, c) current_elem = 
+        let sum_visibles n c (count, continue) = 
+              let new_continue = (c < n) && continue 
+              in  (count + fromEnum continue, new_continue) 
+            left cond  = if cond then foldl (flip $ sum_visibles current_elem) (0, True) else const (0, True)
+            right cond = if cond then foldr (sum_visibles current_elem) (0, True) else const (0, True)
+            nrows = M.nrows m
+            ncols = M.ncols m
+            slide_left   = fst . right (c > 1)     $ M.submatrix r     r     1     (c-1) m
+            slide_right  = fst . left  (c < ncols) $ M.submatrix r     r     (c+1) ncols m
+            slide_top    = fst . right (r > 1)     $ M.submatrix 1     (r-1) c     c     m
+            slide_bottom = fst . left  (r < nrows) $ M.submatrix (r+1) nrows c     c     m
+         in slide_left * slide_right * slide_top * slide_bottom
+
+-- >>> foldr mod 9 $ M.submatrix 3 5 2 2 $ M.fromLists [[1 :: Int,2,3],[4,5,6],[7,8,9]]
+-- submatrix: ending row (5) is out of range. Matrix has 3 rows, and starting row is 3.
+
+
+-- >>> let f n c (count, continue) = let new_continue = (c < n) && continue in (count + fromEnum continue, new_continue)  in foldr ( f 3) (0 :: Int, True) [3,2,1]
+-- (3,False)
+
+-- >>> mod 5 8 
+-- 5
+
 
 main :: IO ()
 main = do
   [part, filepath] <- getArgs
-  input <- lines <$> readFile filepath
+  input <- M.fromLists . fmap (fmap digitToInt) . lines <$> readFile filepath
   if read @Int part == 1
     then do
       print "solution to problem 1 is:"
-      print "not implemented"
+      print $ getSum $ foldMap (Sum . fromEnum) $ isVisible input
     else do
       print "solution to problem 2 is:"
-      print "not implemented"
+      print $ maximum . sceneScore $ input
 
